2ac3ccc8ffab977d9a3f8ab10905c2dfe92d9b0e
walky60710 <walker60710@gmail.com>
2026-02-26 10:50:52 +0530

    homeowrk

---
+++ homework_lab_tets.py
@@ -0,0 +1,11 @@
+
+# print the sum of the values from 1 to 5 as follows. there are more but gib sol to these for now
+
+print(1+2+3+4+5)
+
+n = int(input("Enter an integer: "))
+if n % 2 == 0:
+    print("n is even")
+else:
+    print("n is odd")
+
