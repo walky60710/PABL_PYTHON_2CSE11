d3e687c076d7c45e327c91e0c12bc76b2a3d88df
walky60710 <walker60710@gmail.com>
2026-02-26 11:02:51 +0530

    Update homework_lab_tets.py

--- homework_lab_tets.py
+++ homework_lab_tets.py
@@ -9,3 +9,91 @@ if n % 2 == 0:
 else:
     print("n is odd")
 
+print('*' * 22)
+print('# ' * 11)
+
+name = "David Doe"
+address = "1600 Wilshire Blvd #205, Los Angeles CA 90017"
+print(name, address)
+
+a = int(input())
+b = int(input())
+if a < b:
+    print(a, b)
+else:
+    print(b, a)
+
+adult = int(input("Are you an adult? (1 if you are an adult, 0 if you are minor):"))
+married = int(input("Are you married? (1 if you are married, 0 if you are single):"))
+if adult == 1 and married == 1:
+    print("You are an adult who is married.")
+elif adult == 1 and married == 0:
+    print("You are an adult who is single.")
+elif adult == 0 and married == 1:
+    print("You are a minor who is married.")
+else:
+    print("You are a minor who is single.")
+
+
+for i in range(2, 13):
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(i, ": Prime number")
+    else:
+        print(i, ": Composite number")
+
+
+armstrong = []
+for i in range(100, 1000):
+    a = i // 100
+    b = (i // 10) % 10
+    c = i % 10
+    if a**3 + b**3 + c**3 == i:
+        armstrong.append(i)
+print("Three-digit Armstrong numbers:", *armstrong)
+
+
+l1 = ['I like', 'I love']
+l2 = ['pancake.', 'kiwi juice.', 'espresso.']
+for a in l1:
+    for b in l2:
+        print(a, b)
+
+
+person = {'Name' : 'David Doe', 'Age' : 26, 'Weight' : 82, 'Job' : 'Data scientist'}
+person['Father'] = 'John Doe'
+print(person)
+
+lst = [5, 6, 3, 9, 2, 12, 3, 8, 7]
+for i in range(len(lst) - 1):
+    if lst[i] > lst[i + 1]:
+        lst[i], lst[i + 1] = lst[i + 1], lst[i]
+print(lst)
+
+
+a = [[1, 2], [3, 4], [5, 6]]
+flat = []
+for sub in a:
+    for num in sub:
+        flat.append(num)
+print(flat)
+
+
+maria = {'korean': 94, 'english': 91, 'mathematics': 89, 'science': 83}
+print(sum(maria.values()) / len(maria))
+
+import copy
+school = {'kim': {'age': 16, 'hei': 170, 'grade': 3}, 'lee': {'age': 15, 'hei': 168, 'grade': 2}, 'choi': {'age': 14, 'hei': 173, 'grade': 1}}
+school2 = copy.deepcopy(school)
+print(school is school2)
+
+scores = (('Hyun', 88, 95, 90), ('Kang', 85, 90, 95), ('Park', 70, 90, 80), ('Hong', 90, 90, 95))
+names, eng, math, sci = zip(*scores)
+print(sum(math) / len(math))
+
+
+
